5bd45fff15e8b1eb1f03601e09563e60400ab071
Jake Pitman <jakeseanp@gmail.com>
2019-05-01 10:01:36 +1000

    add documentnotfounderror type to create survey

--- app/graphql/mutations/create_rating_question.rb
+++ app/graphql/mutations/create_rating_question.rb
@@ -1,14 +1,17 @@
 module Mutations
   class CreateRatingQuestion < Mutations::BaseMutation
     argument :title, String, required: true
+    argument :survey_id, ID, required: true
 
     type Types::CreateRatingQuestionResult
 
-    def resolve(title: nil)
+    def resolve(title: nil, survey_id: nil)
       begin
-        RatingQuestion.create!(title: title)
+        RatingQuestion.create!(title: title, survey: Survey.find(survey_id))
       rescue Mongoid::Errors::Validations => e
         puts "RESCUING #{e.class}"
+        e
+      rescue Mongoid::Errors::DocumentNotFound => e
         e
       end
     end
--- app/graphql/types/create_rating_question_result.rb
+++ app/graphql/types/create_rating_question_result.rb
@@ -1,10 +1,12 @@
 module Types
   class CreateRatingQuestionResult < BaseUnion
-    possible_types RatingQuestionType, FailedCreateRatingQuestionResult, MissingArgumentError
+    possible_types RatingQuestionType, FailedCreateRatingQuestionResult, MissingArgumentError, DocumentNotFoundError
     
     def self.resolve_type(object, _context)
       if object.is_a?(Mongoid::Errors::Validations)
         MissingArgumentError
+      elsif object.is_a?(Mongoid::Errors::DocumentNotFound)
+        DocumentNotFoundError
       elsif object.persisted?
         RatingQuestionType
       else
